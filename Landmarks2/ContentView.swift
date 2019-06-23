cae16479598e4d718160a09300321355535b4377
Brett McDonald <bldmcdonald@gmail.com>
2019-06-23 15:56:45 +1200

    Initial Commit

---
+++ Landmarks2/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks2
+//
+//  Created by Brett McDonald on 23/06/19.
+//  Copyright © 2019 Brett McDonald. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
