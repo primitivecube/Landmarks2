667692db10a02f08d11efcbafea91fcc2d86b4e2
Brett McDonald <bldmcdonald@gmail.com>
2019-06-23 16:13:59 +1200

    up to section 5

---
+++ Landmarks2/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  Landmarks2
+//
+//  Created by Brett McDonald on 23/06/19.
+//  Copyright © 2019 Brett McDonald. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage : View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+#if DEBUG
+struct CircleImage_Previews : PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
+#endif
